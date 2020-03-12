e888006224bbdc92063dcab6eb4a8cb899537a19
Your Name <you@example.com>
2020-03-12 17:14:56 +0300

    Add View stub

--- src/view.py
+++ src/view.py
@@ -1,1 +1,13 @@
 """ Module containing the implementation of the graphic output for the game. """
+
+from .model import Model
+from .fighter import Fighter
+from .map import Map
+
+
+class View:
+    def __init__(self):
+        pass
+
+    def draw(self, model: Model):
+        pass
