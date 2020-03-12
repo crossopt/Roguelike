b75b461737942f5102910fb52310c2ff285c4a7c
Your Name <you@example.com>
2020-03-12 19:01:16 +0300

    Simplify assignment

--- src/view.py
+++ src/view.py
@@ -9,6 +9,8 @@ from .world_map import MapTile
 
 wall_color = tcod.white
 path_color = tcod.black
+player_color = tcod.yellow
+text_color = tcod.grey
 
 tile_to_bg = {
     MapTile.BLOCKED: wall_color,
@@ -20,10 +22,13 @@ class View:
     def __init__(self, root_console: Console):
         self.console = root_console
         self.console.default_bg = wall_color
+        self.console.default_fg = text_color
 
     def draw(self, model: Model):
         self.console.clear()
         for i in range(model.map.height):
             for j in range(model.map.width):
                 self.console.bg[i, j] = tile_to_bg[model.map[i][j]]
-        self.console.ch[model.player.position.x, model.player.position.y] = ord("F")
+        player_coords = model.player.position.x, model.player.position.y
+        self.console.ch[player_coords] = ord("F")
+        self.console.fg[player_coords] = player_color
