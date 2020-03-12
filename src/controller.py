0b243577d10bfe69087b0d7f496bba9c0cf0823b
Ivan Shershnev <ivankrut856@yandex.ru>
2020-03-12 17:24:45 +0300

    empty view + some runnable code of controller

--- src/controller.py
+++ src/controller.py
@@ -1,1 +1,26 @@
 """ Module containing the main controller logic for the game. """
+
+import sys
+sys.path.append(".")
+
+from src.model import Model
+from src.view import View
+import tcod.event
+
+class Controller(object):
+
+    def __init__(self, view):
+        self.view = View()
+        self.model = Model()
+        self.isAppRunning = True
+
+    def run_loop(self):
+        for event in tcod.event.wait():
+            print(event.type) 
+
+
+
+if __name__ == "__main__":
+    Controller().run_loop()
+
+
--- src/view.py
+++ src/view.py
@@ -1,1 +1,6 @@
 """ Module containing the implementation of the graphic output for the game. """
+
+class View(object):
+
+	def __init__(self):
+		pass
