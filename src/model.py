4efc6df5fe8dedc0e62fdf42101ed1c3f38e96df
crossopt <s17b2_fedorkina@179.ru>
2020-03-12 21:16:31 +0300

    model renamings

--- src/model.py
+++ src/model.py
@@ -7,12 +7,12 @@ from src.world_map import WorldMap
 
 class Model:
     """ Class encapsulating the state of the game world. """
-    def __init__(self, wmap: WorldMap, player_start: Position):
-        self.map = wmap
+    def __init__(self, initial_map: WorldMap, player_start: Position):
+        self.map = initial_map
         self.player = fighter.Fighter(player_start)
 
-    def set_player_will(self, will):
-        self.player.set_intention(will)
+    def set_player_intention(self, intention):
+        self.player.set_intention(intention)
 
     def get_fighters(self):
         return [self.player]
