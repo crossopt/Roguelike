4bb878e98db8efa5068f83dc34ffec7c46476f7d
crossopt <s17b2_fedorkina@179.ru>
2020-03-12 16:55:01 +0300

    basic fighter and map skeletons

--- src/fighter.py
+++ src/fighter.py
@@ -1,1 +1,16 @@
 """ Module containing the implementation of various in-game fighters. """
+
+
+class Fighter(object):
+    """ Class for storing the various in-game fighter characters. """
+    def __init__(self, initial_position):
+        """ Initializes a fighter with the given initial position. """
+        self.position = initial_position
+
+    def move(self, new_position):
+        """ Changes the fighter's position. """
+        self.position = new_position
+
+    def choose_move(self, world_map):
+        """ Initializes a fighter with the given initial position. """
+        pass
--- src/map.py
+++ src/map.py
@@ -1,1 +1,41 @@
 """ Module containing the implementation of the map generation and storage. """
+
+from enum import Enum
+
+
+class Position(object):
+    """ Class for storing the position of various objects on the map. """
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class MapLoadingException(Exception):
+    """ Exception raised if errors in loading the map occur. """
+
+
+class MapTile(Enum):
+    """ The contents of a given tile on the map. """
+    EMPTY = 0
+    BLOCKED = 1
+    PLAYER = 2
+
+
+class Map(object):
+    """ Class for storing the world map. """
+    def __init__(self):
+        """ Generates a map example of size 5x5. """
+        self.height = 5
+        self.width = 5
+        self.tiles = [[MapTile.EMPTY for i in range(self.width)] for j in range(self.height)]
+
+    def generate(self, height, width):
+        """ Randomly generates a map of size height x width. """
+        pass
+
+    def load(self, file):
+        """ Loads a map from the given file.
+
+        :raise: MapLoadingException if the file's contents could not be parsed into a valid map.
+        """
+        pass
