5237e9570469cd0c42464e550af988888d2c6fad
crossopt <s17b2_fedorkina@179.ru>
2020-03-12 17:03:33 +0300

    very simple model

--- src/model.py
+++ src/model.py
@@ -1,1 +1,11 @@
 """ Module containing the world logic for the game. """
+
+from src.map import Map, Position
+from src.fighter import Fighter
+
+
+class Model(object):
+    """ Class encapsulating the state of the game world. """
+    def __init__(self):
+        self.map = Map()
+        self.player = Fighter(Position(0, 0))
