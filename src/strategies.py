aa74a381d2b9bddb0ab611d61a459a66f9cf49ff
crossopt <s17b2_fedorkina@179.ru>
2020-04-19 16:56:05 +0300

    clean up code, add comments

--- src/strategies.py
+++ src/strategies.py
@@ -1,40 +1,49 @@
-import src.fighter
+""" Module containing strategies for various types of mobs. """
+
 import src.model
 
-def sing(x):
-    if x > 0:
-        return 1
-    elif x < 0:
-        return -1
-    else:
-        return 0
+from abc import abstractmethod
+
+
+def sign(x):
+    return x and (1, -1)[x < 0]
+
 
 class FightingStrategy:
-
+    """ The base class for all fighting strategies for mobs. """
+    @staticmethod
+    @abstractmethod
     def choose_move(current_model: 'src.model.Model', mob: 'src.fighter.Mob'):
+        """ Selects a move for a given mob based on the state of the model world. """
         raise NotImplementedError()
 
-class AggressiveStrategy(FightingStrategy):
 
+class AggressiveStrategy(FightingStrategy):
+    """ An aggressive strategy that always moves towards the player and attacks them. """
+    @staticmethod
     def choose_move(current_model: 'src.model.Model', mob: 'src.fighter.Mob'):
         player = current_model.player
         dx = -player.position.x + mob.position.x
         dy = -player.position.y + mob.position.y
         dx = sign(-dx)
         dy = sign(-dy)
-        return (dx, dy)
+        return dx, dy
+
 
 class CowardlyStrategy(FightingStrategy):
-
+    """ A cowardly strategy that always moves away from the player. """
+    @staticmethod
     def choose_move(current_model: 'src.model.Model', mob: 'src.fighter.Mob'):
         player = current_model.player
         dx = -player.position.x + mob.position.x
         dy = -player.position.y + mob.position.y
         dx = (dx > 0) * 2 - 1
         dy = (dy > 0) * 2 - 1
-        return (dx, dy)
+        return dx, dy
+
 
 class PassiveStrategy(FightingStrategy):
-
+    """ A passive strategy that does not move. """
+    @staticmethod
     def choose_move(current_model: 'src.model.Model', mob: 'src.fighter.Mob'):
-        return (0, 0)
+        return 0, 0
