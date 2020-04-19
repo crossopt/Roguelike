7457f80b3dc5a397922ec300f9a2ab5f4e92856b
crossopt <s17b2_fedorkina@179.ru>
2020-04-19 20:21:10 +0300

    add more tests

--- test/test_fighter.py
+++ test/test_fighter.py
@@ -6,17 +6,38 @@ from src.world_map import Position
 from src.model import Model
 
 
-class TestPlayerMove(unittest.TestCase):
+class TestFighters(unittest.TestCase):
     def setUp(self):
         self.player = fighter.Player(Position(0, 0))
-        self.model = Model(world_map.WorldMap(), self.player, [])
+        self.mob = fighter.Player(Position(2, 2))
+        self.model = Model(world_map.WorldMap(), self.player, [self.mob])
+
+    def testGetAttack(self):
+        self.assertEqual(fighter.MOB_ATTACK, self.mob.get_attack())
+        self.assertEqual(fighter.PLAYER_BASE_ATTACK, self.player.get_attack())
+
+    def testTakeDamage(self):
+        self.assertEqual(fighter.PLAYER_HP, self.player.hp)
+        self.player.take_damage(5)
+        self.assertEqual(fighter.PLAYER_HP - 5, self.player.hp)
+        self.player.take_damage(500)
+        self.assertEqual(0, self.player.hp)
+        self.assertEqual(fighter.MOB_HP, self.mob.hp)
+        self.player.take_damage(1)
+        self.assertEqual(fighter.MOB_HP - 1, self.mob.hp)
+        self.player.take_damage(10)
+        self.assertEqual(0, self.mob.hp)
 
     def testMove_initial_zero(self):
         self.assertEqual(Position(0, 0), self.player.position)
+        self.assertEqual(Position(2, 2), self.mob.position)
 
     def testMove_move(self):
         self.player.move(Position(1, 2))
         self.assertEqual(Position(1, 2), self.player.position)
+
+        self.mob.move(Position(1, 2))
+        self.assertEqual(Position(1, 2), self.mob.position)
 
     def testMove_intentions(self):
         self.player._add_intention(fighter.PlayerIntention.STAY)
