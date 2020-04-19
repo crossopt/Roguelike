58b98c2c4c65b4798111f91cab5db46ac90aed3b
crossopt <s17b2_fedorkina@179.ru>
2020-04-19 20:05:55 +0300

    updated player tests

--- test/test_fighter.py
+++ test/test_fighter.py
@@ -3,36 +3,41 @@ import unittest
 from src import fighter
 from src import world_map
 from src.world_map import Position
+from src.model import Model
 
 
-class TestMove(unittest.TestCase):
+class TestPlayerMove(unittest.TestCase):
     def setUp(self):
-        self.fighter = fighter.Fighter(Position(0, 0))
-        self.map = world_map.WorldMap()
+        self.player = fighter.Player(Position(0, 0))
+        self.model = Model(world_map.WorldMap(), self.player, [])
 
     def testMove_initial_zero(self):
-        self.assertEqual(Position(0, 0), self.fighter.position)
+        self.assertEqual(Position(0, 0), self.player.position)
 
     def testMove_move(self):
-        self.fighter.move(Position(1, 2))
-
-        self.assertEqual(Position(1, 2), self.fighter.position)
+        self.player.move(Position(1, 2))
+        self.assertEqual(Position(1, 2), self.player.position)
 
     def testMove_intentions(self):
-        self.fighter.add_intention(fighter.FighterIntention.STAY)
-        self.assertEqual(Position(0, 0), self.fighter.choose_move(self.map))
+        self.player._add_intention(fighter.PlayerIntention.STAY)
+        self.assertTrue(self.player.has_intention())
+        self.assertEqual(Position(0, 0), self.player.choose_move(self.model))
 
-        self.fighter.add_intention(fighter.FighterIntention.MOVE_UP)
-        self.assertEqual(Position(-1, 0), self.fighter.choose_move(self.map))
+        self.player._add_intention(fighter.PlayerIntention.MOVE_UP)
+        self.assertTrue(self.player.has_intention())
+        self.assertEqual(Position(-1, 0), self.player.choose_move(self.model))
 
-        self.fighter.add_intention(fighter.FighterIntention.MOVE_LEFT)
-        self.assertEqual(Position(0, -1), self.fighter.choose_move(self.map))
+        self.player._add_intention(fighter.PlayerIntention.MOVE_LEFT)
+        self.assertTrue(self.player.has_intention())
+        self.assertEqual(Position(0, -1), self.player.choose_move(self.model))
 
-        self.fighter.add_intention(fighter.FighterIntention.MOVE_DOWN)
-        self.assertEqual(Position(1, 0), self.fighter.choose_move(self.map))
+        self.player._add_intention(fighter.PlayerIntention.MOVE_DOWN)
+        self.assertTrue(self.player.has_intention())
+        self.assertEqual(Position(1, 0), self.player.choose_move(self.model))
 
-        self.fighter.add_intention(fighter.FighterIntention.MOVE_RIGHT)
-        self.assertEqual(Position(0, 1), self.fighter.choose_move(self.map))
+        self.player._add_intention(fighter.PlayerIntention.MOVE_RIGHT)
+        self.assertTrue(self.player.has_intention())
+        self.assertEqual(Position(0, 1), self.player.choose_move(self.model))
 
 
 if __name__ == '__main__':
