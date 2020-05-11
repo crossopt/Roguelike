5fcf8114476a8027769aea6cb58b5ec4d80d918a
crossopt <s17b2_fedorkina@179.ru>
2020-05-11 16:58:35 +0300

    add save tests

--- test/test_saves.py
+++ test/test_saves.py
@@ -1,8 +1,8 @@
+import os
 import unittest
 
-from src import fighter
+from src import controller
 from src import world_map
-from src.controller import Controller
 from src.strategies import PassiveStrategy
 from src.world_map import Position
 from src.model import Model
@@ -10,16 +10,33 @@ from src.model import Model
 
 class TestSaves(unittest.TestCase):
     def setUp(self):
-        self.controller = Controller(_test_args=[])
+        self.controller = controller.Controller(_test_args=[])
+        self.controller._remove_saved_game()
 
     def testSaveActuallySaves(self):
-        pass
+        self.assertFalse(os.path.isfile(controller.SAVE_FILE_NAME))
+        self.controller._save_game()
+        self.assertTrue(os.path.isfile(controller.SAVE_FILE_NAME))
 
     def testSaveSavesOnlyOnce(self):
-        pass
+        self.controller._save_game()
+        with open(controller.SAVE_FILE_NAME, 'r') as file:
+            old_save = file.read()
+        self.controller.model = Model()
+        self.controller._save_game()
+        with open(controller.SAVE_FILE_NAME, 'r') as file:
+            new_save = file.read()
+        self.assertNotEqual(old_save, new_save)
 
     def testPlayerDeathForbidsSaves(self):
-        pass
+        self.assertFalse(os.path.isfile(controller.SAVE_FILE_NAME))
+        self.controller._save_game()
+        self.assertTrue(os.path.isfile(controller.SAVE_FILE_NAME))
+        self.controller._remove_saved_game()
+        self.controller.player_died = True
+        self.assertFalse(os.path.isfile(controller.SAVE_FILE_NAME))
+        self.controller._save_game()
+        self.assertFalse(os.path.isfile(controller.SAVE_FILE_NAME))
 
 
 if __name__ == '__main__':
