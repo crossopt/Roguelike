fe6b382f9fbf7ef21e8717996e267e49b1365390
Your Name <you@example.com>
2020-05-12 00:06:34 +0300

    Mor

--- roguelike-client.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-from src.client_controller import ClientController
-
-ClientController().run_loop()
--- roguelike.py
+++ roguelike.py
@@ -5,7 +5,7 @@ from src.client_controller import OfflineController, ClientController
 
 parser = ArgumentParser(description='A simple console-based rogue-like game.')
 parser.add_argument('--map_path', type=str, help='path to map file to load')
-parser.add_argument('--new_game', type=bool, dest='new_game_demanded', default=False)
+parser.add_argument('--new_game', dest='new_game_demanded', action='store_true')
 parser.add_argument('--address', type=str, default='127.0.0.1')
 parser.add_argument('--port', type=int, default=50051)
 parser.add_argument('--connect', action='store_true')
